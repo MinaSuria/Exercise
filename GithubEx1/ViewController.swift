b64efdcf068a21ddce72082c5b3c1f87156adb3d
MinaSuria <mina.suria@effulgentsolutions.com>
2017-02-02 14:17:37 +0800

    hello world4

--- GithubEx1/ViewController.swift
+++ GithubEx1/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
         print("hello world")
         print("hello world2")
         print("hello world3")
+        print("hello world4")
     }
 
     override func didReceiveMemoryWarning() {
